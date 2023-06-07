87871fff3bce6c293f7bdd325e023c6dd11ad8f6
ruslan tasmukhanov <ruslahn.q@gmail.com>
2023-06-07 09:29:18 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MainLayout from "./layouts/MainLayout";
-import Zagruzka from "./components/Zagruzka";
+// import Zagruzka from "./components/Zagruzka";
 import Grid from "./components/Grid";
 
 function App() {
@@ -16,6 +16,71 @@ function App() {
     { id: 9, text: "" },
   ]);
   const [user, setUser] = useState(true);
+  const [gameOver, setGameOver] = useState(null);
+  
+  function clearGrid(){
+    setGameOver(user ? "Zero" : "Cross");
+    setUser(true);
+    setGrid([
+      { id: 1, text: "" },
+      { id: 2, text: "" },
+      { id: 3, text: "" },
+      { id: 4, text: "" },
+      { id: 5, text: "" },
+      { id: 6, text: "" },
+      { id: 7, text: "" },
+      { id: 8, text: "" },
+      { id: 9, text: "" },
+    ]);
+  }
+
+  function isDraw(){
+    let count = 0;
+    for(let i = 0; i < 9; i++){
+      if(grid[i].text != "") count++;
+    }
+
+    if(count == 9) return true;
+    return false;
+  }
+
+  function checkWin(){
+    // console.log("checkwin")
+    // console.log(grid);
+
+    for(let i = 0; i < 9; i += 3){
+      if(grid[i].text != "" && grid[i].text == grid[i + 1].text && grid[i + 1].text == grid[i + 2].text){
+        clearGrid();
+      }
+    }
+    for(let i = 0; i < 3; i++){
+      if(grid[i].text != "" && grid[i].text == grid[i + 3].text && grid[i + 3].text == grid[i + 6].text){
+        clearGrid();
+      }
+    }
+    if(grid[0].text != "" && grid[0].text == grid[4].text && grid[4].text == grid[8].text) {
+      clearGrid();
+    }
+    else if(grid[2].text != "" && grid[2].text == grid[4].text && grid[4].text == grid[6].text) {
+      clearGrid();
+    }
+    else if(isDraw()){
+      setGameOver("Draw");
+      setUser(true);
+      setGrid([
+        { id: 1, text: "" },
+        { id: 2, text: "" },
+        { id: 3, text: "" },
+        { id: 4, text: "" },
+        { id: 5, text: "" },
+        { id: 6, text: "" },
+        { id: 7, text: "" },
+        { id: 8, text: "" },
+        { id: 9, text: "" },
+      ]);
+    }
+    
+  }
 
   function handlePlay(elementID) {
     //NO NO push splice pop shift unshift
@@ -27,8 +92,11 @@ function App() {
     });
     setUser(!user);
     setGrid(newGrid);
+    // checkWin();
   }
+  checkWin();
 
+  // console.log(grid);
   // if (!user) {
   //   return (
   //     <MainLayout>
@@ -39,7 +107,7 @@ function App() {
 
   return (
     <MainLayout>
-      <Grid grid={grid} handlePlay={handlePlay} />
+      <Grid grid={grid} gameOver={gameOver} handlePlay={handlePlay} />
     </MainLayout>
   );
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,17 +1,22 @@
 export default function Grid(props) {
+  const {gameOver} = props;
+  console.log(gameOver)
   return (
-    <div className="grid grid-cols-3 m-auto w-fit">
-      {props.grid.map((element) => (
-        <div key={element.id} className="relative group">
-          <div className="absolute transition duration-500 opacity-75 -inset-1 bg-gradient-to-r from-slate-950 via-red-600 to-stone-700 blur group-hover:opacity-100"></div>
-          <button
-            onClick={() => props.handlePlay(element.id)}
-            className="relative w-24 h-24 transition duration-500 bg-slate-950 hover:bg-slate-800"
-          >
-            {element.text}
-          </button>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-3 m-auto w-fit">
+        {props.grid.map((element) => (
+          <div key={element.id} className="relative group">
+            <div className="absolute transition duration-500 opacity-75 -inset-1 bg-gradient-to-r from-slate-950 via-red-600 to-stone-700 blur group-hover:opacity-100"></div>
+            <button
+              onClick={() => props.handlePlay(element.id)}
+              className="relative w-24 h-24 transition duration-500 bg-slate-950 hover:bg-slate-800"
+            >
+              {element.text}
+            </button>
+          </div>
+        ))}
+      </div>
+      {gameOver && gameOver != "Draw" ? <div style={{display:"flex", justifyContent: "center", color: "white"}}> Last winner is {gameOver}</div> : <div style={{display:"flex", justifyContent: "center", color: "white"}}> Draw </div>}
+    </>
   );
 }
